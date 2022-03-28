2db6faac3cc2b199f158f4cf9aa89141df8a7d38
rodrigosalles7 <rodrigoolisalles@hotmail.com>
2022-03-28 11:47:23 -0300

    Cria os caminhos de URL para os endpoints.

---
+++ proj/django_proj/proj/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from proj import views
+
+urlpatterns = [
+    path("", views.ListProjAPIView.as_view(), name="proj_list"),
+    path("create/", views.CreateProjAPIView.as_view(), name="proj_create"),
+    path("update/<int:pk>/", views.UpdateProjAPIView.as_view(), name="update_proj"),
+    path("delete/<int:pk>/", views.DeleteProjAPIView.as_view(), name="delete_proj")
+]
