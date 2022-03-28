4943bec564cd160c8abf1802ad021e5c854bfa62
rodrigosalles7 <rodrigoolisalles@hotmail.com>
2022-03-28 12:01:57 -0300

    Documenta os endpoints para facilitar a utilização.

--- proj/django_proj/django_proj/urls.py
+++ proj/django_proj/django_proj/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/proj/',include("proj.urls")),
+    path('api/v1/proj/', include("proj.urls")),
+    path('docs/', include_docs_urls(title='Proj Api')),
 ]
