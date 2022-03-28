dc0e3894d01968310068b27b2e82c86bfe43d82c
rodrigosalles7 <rodrigoolisalles@hotmail.com>
2022-03-28 11:44:52 -0300

    Cria o ProjAPIView.

--- proj/django_proj/proj/views.py
+++ proj/django_proj/proj/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.generics import CreateAPIView
+from rest_framework.generics import DestroyAPIView
+from rest_framework.generics import UpdateAPIView
+from proj.serializers import ProjSerializer
+from proj.models import Proj
 
 # Create your views here.
+
+
+class ListProjAPIView(ListAPIView):
+    queryset = Proj.objects.all()
+    serializer_class = ProjSerializer
+
+
+class CreateProjAPIView(CreateAPIView):
+    queryset = Proj.objects.all()
+    serializer_class = ProjSerializer
+
+
+class UpdateProjAPIView(UpdateAPIView):
+    queryset = Proj.objects.all()
+    serializer_class = ProjSerializer
+
+
+class DeleteProjAPIView(DestroyAPIView):
+    queryset = Proj.objects.all()
+    serializer_class = ProjSerializer
